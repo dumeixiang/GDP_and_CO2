b8b0340548aa0bc5c38e8b02084edbaca7b4c094
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-09-07 18:27:23 +0000

    jiwon

---
+++ file.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+data = pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv")
+data1 = pd.DataFrame(data)
+#print(data1.head())
+columns = data1[['Mortality rate, infant (per 1,000 live births)','GDP per capita (constant 2010 US$)','Country Name']]
+
+plt.plot('Mortality rate, infant (per 1,000 live births)', 'GDP per capita (constant 2010 US$)', data=data1)
+plt.show()
+
